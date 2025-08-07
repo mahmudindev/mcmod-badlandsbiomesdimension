b59a906a5ac4ea29b15fd6683322a959aac77722
M. Mahmudin <42290713+mahmudindev@users.noreply.github.com>
2025-08-07 17:18:52 +0700

    Updates to 1.21

---
+++ neoforge/src/main/java/com/github/mahmudindev/mcmod/badlandsbiomesdimension/neoforge/BadlandsBiomesDimensionNeoForge.java
@@ -0,0 +1,12 @@
+package com.github.mahmudindev.mcmod.badlandsbiomesdimension.neoforge;
+
+import com.github.mahmudindev.mcmod.badlandsbiomesdimension.BadlandsBiomesDimension;
+import net.neoforged.fml.common.Mod;
+
+@Mod(BadlandsBiomesDimension.MOD_ID)
+public final class BadlandsBiomesDimensionNeoForge {
+    public BadlandsBiomesDimensionNeoForge() {
+        // Run our common setup.
+        BadlandsBiomesDimension.init();
+    }
+}
