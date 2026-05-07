d862fa338f3ae272c2bc3d62d13c1e0994e76755
M. Mahmudin <42290713+mahmudindev@users.noreply.github.com>
2026-05-08 00:44:33 +0700

    Updates to 26.1

--- common/src/main/java/com/github/mahmudindev/mcmod/badlandsbiomesdimension/BadlandsBiomesDimension.java
+++ common/src/main/java/com/github/mahmudindev/mcmod/badlandsbiomesdimension/BadlandsBiomesDimension.java
@@ -1,7 +1,14 @@
 package com.github.mahmudindev.mcmod.badlandsbiomesdimension;
 
+import com.github.mahmudindev.mcmod.badlandsbiomesdimension.platform.Services;
+import com.github.mahmudindev.mcmod.badlandsbiomesdimension.platform.services.IPlatformHelper;
+import com.mojang.logging.LogUtils;
+import org.slf4j.Logger;
+
 public final class BadlandsBiomesDimension {
     public static final String MOD_ID = "badlandsbiomesdimension";
+    public static final Logger LOGGER = LogUtils.getLogger();
+    public static final IPlatformHelper PLATFORM = Services.PLATFORM;
 
     public static void init() {
         // Write common init code here.
